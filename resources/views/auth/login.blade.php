cde93f0cdc667665ef3b9ab830f8a27d1af148fb
shibayama <sia48801219@icloud.com>
2022-03-16 16:54:10 +0900

    修正漏れを再修正した

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,23 +32,21 @@
                 </label>
             </div>--}}
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
-                    {{ '新規登録はこちら' }}
-                </a>
-            </div>
-
-            <div>
+            <div class="flex items-center justify-end mt-4 mb-4">
                 <x-jet-button class="ml-4 mt-4">
                     {{ __('Log in') }}
                 </x-jet-button>
             </div>
+                
+            @if (Route::has('password.request'))
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+            @endif
+
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 ml-4" href="{{ route('register') }}">
+                {{ '新規登録はこちら' }}
+            </a>
         </form>
     </x-jet-authentication-card>
 </x-guest-layout>
